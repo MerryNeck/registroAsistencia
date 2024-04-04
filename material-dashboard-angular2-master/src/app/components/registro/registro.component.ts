5c41c44fe1184930a9ace042e5021ee05241af2a
MerryNeck <liliansonia777@gmail.com>
2024-04-04 18:10:20 -0400

    origin5

--- material-dashboard-angular2-master/src/app/components/registro/registro.component.ts
+++ material-dashboard-angular2-master/src/app/components/registro/registro.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Area }from "models/area.model";
 import { Rol } from 'models/rol.model';
-import {FormGroup, FormsModule,FormControl} from "@angular/forms"
+import { Usuario } from 'models/usuario.model';
+import {FormGroup, FormsModule,FormControl, Validators, NgForm} from "@angular/forms"
 import {  RegistroService } from 'services/registro.service';
 
 @Component({
@@ -11,36 +12,65 @@ import {  RegistroService } from 'services/registro.service';
   
 })
 export class RegistroComponent implements OnInit{
-
-  usuarioForm: FormGroup;
+ /* usuarioForm: FormGroup;
   areas: Area[] = [];
   roles: Rol[] = [];
   error: string;
   selectedArea: Area; 
   selectedRol: Rol;
+  usuarios:Usuario[] =[];
+ nuevoUsuario: Usuario= new Usuario(0, '', '', '','', '', '');
+ editandoUsuario: Usuario| null = null;
+
+  usuario=[{
+    id_area: 1,
+    tipo_area: 'admin',
+},{
+  id_area: 2,
+  tipo_area: 'admin',
+},{
+  id_area: 3,
+  tipo_area: 'administracion',
+}]
   constructor(private usuarioService: RegistroService) {}
 
- ngOnInit() {
-  //const modalRef = this.modalService.open(this.modalMensaje);
-    /*this.usuarioForm = new FormGroup({
-      nombre: new FormControl(''),
-      apellido_paterno: new FormControl(''),
-      apellido_materno: new FormControl(''),
-      ci: new FormControl(''),
-      idArea: new FormControl(null),
-      idRol: new FormControl(null)
-    })*/
+ ngOnInit():void {}
+   /* RegistrarNuevoUsuario(form:NgForm):void{
+      if (form.valid) {
+        const { ci, anticipo } = form.value;
+        this.nuevoUsuario.id_usuario = ci;
+        this.nuevoUsuario.anticipos = anticipo;
+        this.usuarioService.registra(this.nuevoAnticipo)
+          .subscribe(anticipo => {
+            this.anticipos.push(anticipo);
+            this.nuevoAnticipo = new Anticipo(0, '', '', '', 0, 0);
+            this.usuarioService.getAreas().subscribe({
+              next: (areas) => {
+                this.areas = areas;
+              },
+              error: (error) => {
+                console.error('Error al cargar las áreas', error);
+                
+              }
+            });
+            this.usuarioService.getRoles().subscribe({
+              next: (roles) => {
+                this.roles = roles;
+              },
+              error: (error) => {
+                console.error('Error al cargar los roles', error);
+        
+                }
+               
+            });
+            form.reset();
+          });
+      }
+    }
+    
 
-    this.usuarioService.getAreas().subscribe(areas => {
-      this.areas = areas;
-    });
 
-    this.usuarioService.getRoles().subscribe(roles => {
-      this.roles = roles;
-    });
-  }
-
-  onSubmit() {
+  insertarUsuario() {
     const usuario = this.usuarioForm.value;
     this.usuarioService.insertUsuario(usuario).subscribe(() => {
       // Usuario registrado correctamente
@@ -49,5 +79,5 @@ export class RegistroComponent implements OnInit{
     }, error => {
       this.error = error.message;
     });
-  }
+  }*/
 }
