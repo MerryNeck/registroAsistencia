57def0cbb2ac5609ab3eab54f75bafb325f47ba6
Merry Neck <liliansonia777@gmail.com>
2024-04-10 23:21:42 -0400

    oficial16

--- material-dashboard-angular2-master/src/services/excel.service.ts
+++ material-dashboard-angular2-master/src/services/excel.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Excel } from '../models/excel.model';
 import { environment } from 'environments/environment';
@@ -11,9 +11,14 @@ export class ExcelService {
   private apiUrl = environment.backend.excel; 
   constructor(private http: HttpClient) { }
   subirArchivo(archivo: File): Observable<any> {
+    const token = localStorage.getItem('token');
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
     const formData = new FormData();
     formData.append('archivo', archivo);
     
-    return this.http.post(this.apiUrl, formData);
+    return this.http.post(this.apiUrl, formData, {headers});
   }
 }
